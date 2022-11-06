05e93e942c0e8948e58bf29277250c4def193b19
rolandopiadbeloyjr <102139566+rolandopiadbeloyjr@users.noreply.github.com>
2022-11-06 23:22:06 +0800

    Add files via upload

---
+++ 8. React Component Instantiation/App.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+const list = [
+{
+title: 'React',
+url: 'https://reactjs.org/',
+author: 'Jordan Walke',
+num_comments: 3,
+points: 4,
+objectID: 0,
+},
+{
+title: 'Redux',
+url: 'https://redux.js.org/',
+author: 'Dan Abramov, Andrew Clark',
+num_comments: 2,
+points: 5,
+objectID: 1,
+},
+];
+
+function App() {
+  return (
+  <div>
+  <h1>My Hacker Stories</h1>
+  <label htmlFor="search">Search: </label>
+  <input id="search" type="text" />
+  <hr />
+  {/* render the list here */}
+  {/* and by the way: that's how you do comments in JSX */}
+  </div>
+  );
+  }
+
+  export default App;
